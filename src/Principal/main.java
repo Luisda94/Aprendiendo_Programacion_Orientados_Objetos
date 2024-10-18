f97f8787ec84331f8b3d688cff6b8cb7a8ecb4b4
Luisda <luisda915@gmail.com>
2024-10-18 15:40:03 -0300

    se crea nuevo objeto celular

--- src/Principal/main.java
+++ src/Principal/main.java
@@ -5,7 +5,7 @@ import Aprendiendo_POO.TelefonoMovil;
 public class main {
 	//creacion de instancia(objeto ) a partir de la clase ''Aprendiendo_POO" que deberai llamarse modelo, 
 	//Es decir creamos el objeto
-	
+		
 	//creamoas esta clase para poder instanciar es decir para poder crear los objetos, en este caso seria los nuevos telefonos moviles.
 	//Nota la clase modelo fue creada primreo
     public static void main(String[] args) {
@@ -13,10 +13,17 @@ public class main {
     	//Ahora vamos a crear instanciar un nuevo objeto 
     	TelefonoMovil celular1 = new TelefonoMovil("Nokia","N23",2, "azul");
     	System.out.println(celular1.getMarca());//get nos permite acceder al metodo
-    
+    	
+    	//crearemos otro objeto nuevo
+    	TelefonoMovil celular2 = new TelefonoMvoil("Motorola,G54",3,"negro");
+    //	
+    	
+    	
     //ahora vamos a probar el metodo set para modificar en este caso el modelo del telefono
     	celular1.setModelo("modelo N95");
     	System.out.println("mostramos la marca del celular creado :" + celular1.getModelo());
-    
+
+    	
+    	
     }
 }
