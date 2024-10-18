6108ec3e586c7461eb293c0c3b85428ed3db8b64
Luisda <luisda915@gmail.com>
2024-10-18 15:18:17 -0300

    Se crea un nuevo obj telefono

--- src/Principal/main.java
+++ src/Principal/main.java
@@ -2,11 +2,14 @@ package Principal;
 
 public class main {
 	//creacion de instancia(objeto ) a partir de la clase ''Aprendiendo_POO" que deberai llamarse modelo, 
+	//Es decir creamos el objeto
+	
+	//creamoas esta clase para poder instanciar es decir para poder crear los objetos, en este caso seria los nuevos telefonos moviles.
 	//Nota la clase modelo fue creada primreo
     public static void main(String[] args) {
     	
-    	
-    	
+    	//Ahora vamos a crear instanciar un nuevo objeto 
+    	TelefonoMovil celular1 = new TelefonoMovil("Nokia","N23",2, "azul");
     	
     }
 }
