6e0d7caea8c3a471df3fb9cb1d103d02ab38b781
Luisda <luisda915@gmail.com>
2024-10-18 15:08:29 -0300

    Se empieza a crear el objeto para luego instanciar

--- src/Principal/main.java
+++ src/Principal/main.java
@@ -1,5 +1,12 @@
 package Principal;
 
 public class main {
-
+	//creacion de instancia(objeto ) a partir de la clase ''Aprendiendo_POO" que deberai llamarse modelo, 
+	//Nota la clase modelo fue creada primreo
+    public static void main(String[] args) {
+    	
+    	
+    	
+    	
+    }
 }
